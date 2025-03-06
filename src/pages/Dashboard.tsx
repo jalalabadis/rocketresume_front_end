3c6def28e22a239c5d2c08979a54230069eeaab2
Jalalabadi <jalalabadijk@gmail.com>
2025-03-06 13:40:07 +0600

    upV2.8

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1633,7 +1633,7 @@ const Dashboard: React.FC = () => {
       </div>
 
  <div className="flex w-100 mt-3 items-center justify-center">
- <div className="flex flex-col md:flex-row w-[96%] md:w-[50%] items-center justify-center md:justify-start gap-1 md:gap-8 text-center bg-gradient-to-r from-blue-500 to-indigo-500 px-6 py-2 rounded-lg shadow-lg">
+ <div className="flex flex-col md:flex-row w-[96%] md:w-[95%] items-center justify-center md:justify-start gap-1 md:gap-8 text-center bg-gradient-to-r from-blue-500 to-indigo-500 px-6 py-2 rounded-lg shadow-lg">
  <span className="text-white text-sm sm:text-lg md:text-xl font-semibold mb-2">
   Take a look at our best work and get inspired!
 </span>
